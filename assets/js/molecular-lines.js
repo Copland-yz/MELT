435a7b9d07697cbb4c7ab6f387831821d58a7131
Copland-yz <coplandyong@gmail.com>
2025-11-07 16:25:19 -0600

    better legend mark

--- assets/js/molecular-lines.js
+++ assets/js/molecular-lines.js
@@ -921,8 +921,8 @@ function createSpectrumChart(continuousData, discreteLines, nullIntensityLines) 
                             },
                             padding: 15,
                             usePointStyle: false,
-                            boxWidth: 50,
-                            boxHeight: 3,
+                            boxWidth: 40,
+                            boxHeight: 15,
                             filter: function(legendItem, chartData) {
                                 // Hide datasets that have showInLegend: false
                                 const dataset = chartData.datasets[legendItem.datasetIndex];
